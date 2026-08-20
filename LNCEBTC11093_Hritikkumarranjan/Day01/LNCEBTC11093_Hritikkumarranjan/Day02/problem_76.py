4cdd6b2f02271202f7bf452161bfb641281e6b77
Hritik Kumar Ranjan <hritikkumarranjan3@gmail.com>
2026-08-20 22:29:06 +0530

    Create problem_76.py

---
+++ LNCEBTC11093_Hritikkumarranjan/Day01/LNCEBTC11093_Hritikkumarranjan/Day02/problem_76.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minWindow(self, s, t):
+        if not s or not t:
+            return ""
+
+        need = {}
+
+        for ch in t:
+            need[ch] = need.get(ch, 0) + 1
+
+        window = {}
+        left = 0
+        count = 0
+        min_len = float("inf")
+        start = 0
+
+        for right in range(len(s)):
+            ch = s[right]
+            window[ch] = window.get(ch, 0) + 1
+
+            if ch in need and window[ch] <= need[ch]:
+                count += 1
+
+            while count == len(t):
+                if right - left + 1 < min_len:
+                    min_len = right - left + 1
+                    start = left
+
+                left_ch = s[left]
+                window[left_ch] -= 1
+
+                if left_ch in need and window[left_ch] < need[left_ch]:
+                    count -= 1
+
+                left += 1
+
+        if min_len == float("inf"):
+            return ""
+
+        return s[start:start + min_len]
